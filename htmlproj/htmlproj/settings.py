2b05ba899f0ec8d5e00c2962dffaf7f1fbe0288e
Данила <dkorovkin2003@mail.ru>
2024-03-21 18:30:17 +0700

    added database

--- htmlproj/htmlproj/settings.py
+++ htmlproj/htmlproj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +75,14 @@ WSGI_APPLICATION = 'htmlproj.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'HTML_gen',
+'USER': 'postgres',
+'PASSWORD': '12345',
+'HOST': '127.0.0.1',
+'PORT': '5432'
+}
 }
 
 
@@ -121,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
